647e85a9009ecf8bd75b97af40c992e6c9cfb7e1
ВашÐBohdan Kravets <@kravetsbodj@gmail.com>
2024-11-05 15:51:04 +0200

    Develop sraper be faster

--- parse_goat/spiders/goat_spider.py
+++ parse_goat/spiders/goat_spider.py
@@ -6,7 +6,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 import time
 
-
 class GoatSpider(scrapy.Spider):
     name = "goat_spider"
     allowed_domains = ["goat.com"]
@@ -16,7 +15,11 @@ class GoatSpider(scrapy.Spider):
         super(GoatSpider, self).__init__(*args, **kwargs)
 
         chrome_options = Options()
-        chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
+        chrome_options.add_argument(
+            "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+        )
+        chrome_options.add_argument("--disable-gpu")
+        chrome_options.add_argument("--no-sandbox")
         self.driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
 
     def start_requests(self):
@@ -34,26 +37,31 @@ class GoatSpider(scrapy.Spider):
             return
 
         driver.get(self.start_urls[0])
-
-        time.sleep(10)
+        time.sleep(5)
 
         try:
             accept_button = driver.find_element(By.XPATH, "//button[contains(text(), 'Accept all cookies')]")
             accept_button.click()
-            time.sleep(2)
+            time.sleep(10)
         except Exception as e:
             self.logger.warning("Accept cookies button not found or could not be clicked.")
             self.logger.warning(e)
 
         last_height = driver.execute_script("return document.body.scrollHeight")
+        scroll_attempts = 0
 
         while True:
             driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-            time.sleep(10)
+            time.sleep(2)
 
             new_height = driver.execute_script("return document.body.scrollHeight")
             if new_height == last_height:
-                break
+                scroll_attempts += 1
+                if scroll_attempts >= 15:
+                    break
+            else:
+                scroll_attempts = 0
+
             last_height = new_height
 
         products = driver.find_elements(By.CSS_SELECTOR, "div[data-qa='grid_cell_product']")
